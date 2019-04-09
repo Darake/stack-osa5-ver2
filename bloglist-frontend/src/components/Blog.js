e9065d9cb4dea01796561fd9d7708e704cf35460
Gert Adamson <adamson.gert@hotmail.com>
2019-04-09 19:30:55 +0300

    Tehtävä 5.6

--- bloglist-frontend/src/components/Blog.js
+++ bloglist-frontend/src/components/Blog.js
@@ -1,8 +1,29 @@
-import React from 'react'
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>
-)
+import React, { useState } from 'react'
+
+const Blog = ({ blog }) => {
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+
+  const [expanded, setExpanded] = useState(false)
+
+  const showWhenExpanded = { display: expanded ? '' : 'none' }
+
+  return (
+    <div style={blogStyle}>
+      <div onClick={() => setExpanded(!expanded)}>
+        {blog.title} by {blog.author}
+      </div>
+      <div style={showWhenExpanded}>
+        {blog.url} <br/>
+        {blog.likes} likes <button>like</button> <br/>
+        added by {blog.user.name}
+      </div>
+    </div>
+)}
 
 export default Blog
